a5698484138144642cf363e95a5a858bc1f12e8c
Boris Dolgov <boris@dolgov.tech>
2020-07-17 23:41:55 +0300

    Add client func GetRepositoryList
    Function GetRepository list call APICallCatalog and received json object
    with array of repositories stored in docker registry.

--- client/client.go
+++ client/client.go
@@ -1,11 +1,30 @@
 package client
 
 import (
+	"fmt"
+	"net/http"
 	"net/url"
 )
 
 // A Registry represents docker registry server
 //     URL is a registry server url
 type Registry struct {
-	URL *url.URL
+	APIVersion string
+	URL        *url.URL
+}
+
+// GetRepositoryList returns list of existings repositories
+func (r *Registry) GetRepositoryList() {
+	method := APICallCatalog.method
+	url := fmt.Sprintf("%v/%v/%v", r.URL, r.APIVersion, APICallCatalog.path)
+	request, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(response)
 }
