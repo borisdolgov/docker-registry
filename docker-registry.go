4adf7b1a568978c4f2842a9a5066e9fc13f6f87e
Boris Dolgov <boris@dolgov.tech>
2020-07-18 01:26:33 +0300

    Delete APIVersion field from Client struct

--- docker-registry.go
+++ docker-registry.go
@@ -9,14 +9,13 @@ import (
 // A Client represents docker registry server
 //     URL is a registry server url
 type Client struct {
-	APIVersion string
-	URL        *url.URL
+	URL *url.URL
 }
 
 // GetRepositoryList returns list of existings repositories
 func (c *Client) GetRepositoryList() {
 	method := APICallCatalog.method
-	url := fmt.Sprintf("%v/%v/%v", c.URL, c.APIVersion, APICallCatalog.path)
+	url := fmt.Sprintf("%v/v2/%v", c.URL, APICallCatalog.path)
 	request, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		fmt.Println(err)
