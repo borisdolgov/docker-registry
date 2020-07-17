478ad154a273ded09ddfc172230528fcb83638f7
Boris Dolgov <boris@dolgov.tech>
2020-07-17 19:04:56 +0300

    Add api call  "APICallCatalog"

--- client/apiends.go
+++ client/apiends.go
@@ -1,7 +1,12 @@
 package client
 
+import "net/http"
+
 // A RegAPIEndpoint represents docker registry API endpoint
 type RegAPIEndpoint struct {
 	method string
 	path   string
 }
+
+// APICallCatalog provides list of repositories stored in docker registry
+var APICallCatalog = RegAPIEndpoint{method: http.MethodGet, path: "_catalog"}
